4cf5bd7f0c186a7d12bfa9fd1925a01f93ae5505
lmxy <32830461+lmxy@users.noreply.github.com>
2022-03-31 00:34:45 +0800

    Update and rename main to main.py
    change to save each time

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def getNames(): #  到指定路径处names.txt获得待查询企业
 def getContents_1(url):
     res = requests.get(url=url, headers=headers, cookies=cookies)
     soup = BeautifulSoup(res.text, 'html.parser')
-    print(soup)
+    # print(soup)
     contents = {
         '统一社会信用代码': soup.select('tr:nth-child(1) > td:nth-child(2) > div > span.copy-value')[0].text,
         '企业名称': soup.select('tr:nth-child(1) > td:nth-child(4) > div > span.copy-value')[0].text.strip(),
@@ -61,39 +61,42 @@ def getContents_1(url):
 
 
 cookies = get_cookies()
-print(cookies)
+# print(cookies)
 # cookies = 'qcc_did=93869394-c8bf-4638-bc39-0fba12337cdd; UM_distinctid=17fac24b389644-0ff34c45aed19a-9771539-240000-17fac24b38a75c; QCCSESSID=a7177a247e40dfa8fd690a3e9b; acw_tc=3ad8379916486553394534936edf64dfbfad19ce8e4d5fc1077c2cfd0a; CNZZDATA1254842228=593471583-1647850686-https%253A%252F%252Fwww.google.com%252F%7C1648649887'
 urls = []
 i = 1
 # names = ['佳源科技股份有限公司']
-names = ['江苏省建筑工程质量检测中心有限公司', '佳源科技股份有限公司']
+# names = ['江苏省建筑工程质量检测中心有限公司', '佳源科技股份有限公司']
 
-# names = getNames()
-
+names = getNames()
+contents = []
 for name in names:
     print(name)
-    res = requests.get(url=urlEcode(name), headers=headers, cookies=cookies)
+    res = requests.get(url=urlEcode(name), headers=headers, cookies=get_cookies())
     # print(res.text)
     url_0 = getUlr(res)
     print('开始爬取第{}条数据'.format(i), url_0)
-    urls.append(url_0)
-    print('waiting-------------------------5s')
-    time.sleep(15) # 已验证3秒被封，30秒安然无恙，10秒被封，验证15秒
+    # urls.append(url_0)
+    print('waiting-------------------------15s')
+    time.sleep(30) # 已验证15秒被封
+    content = getContents_1(url_0)
+    print(f'爬取了第{i}条数据', content)
+    contents.append(content)
+    print('waiting for more-------------------------10s')
+    time.sleep(15)
     i += 1
 
-print(urls)
-url = urls[0]
-contents = []
-for url in urls:
-    content = getContents_1(url)
-    contents.append(content)
-    time.sleep(15)
-
 df = pd.DataFrame.from_dict(contents)
-
 print(df)
+df.to_excel('企业基本信息.xlsx', index=False)
 
-df.to_excel('企业基本信息.xlsx')
+
+
+
+
+
+
+
 
 
 
